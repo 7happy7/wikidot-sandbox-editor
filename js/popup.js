7885145bf926d2dc477b75c8b96ef9aec510eb4c
7happy7 - JP <49482246+7happy7@users.noreply.github.com>
2020-06-27 21:09:46 +0900

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,47 @@
 var reload = document.querySelector("#reload");
 var update = document.querySelector("#update");
+
 var list = document.querySelector("#list");
 
+var word = document.querySelector("#word");
+var add = document.querySelector("#new");
+
+
 reload.onclick = function() {
-    rel();
+    _rel();
 }
-function rel() {
+word.oninput = function() {
+    if(word.value.length) {
+        add.removeAttribute("disabled");
+    }else {
+        add.setAttribute("disabled", "");
+    }
+}
+add.onclick = function() {
+    if(!word.value.length) {
+        return;
+    }
+    var li = document.createElement("li");
+    li.setAttribute("class", "new");
+    list.appendChild(li);
+    var sp = document.createElement("span");
+    li.appendChild(sp);
+    var bt = document.createElement("button");
+    li.appendChild(bt);
+    sp.innerText = word.value;
+    bt.innerText = "×";
+    bt.onclick = function(e) {
+        var a = e.target;
+        while(a.tagName !== "LI") {
+            a = a.parentNode;
+        }
+        a.setAttribute("class", "del");
+    }
+    add.setAttribute("disabled", "");
+    word.value = "";
+}
+
+function _rel() {
     chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
         chrome.tabs.sendMessage(tabs[0].id, {type: 'get'}, function(item){
             list.innerHTML = "";
@@ -25,7 +61,7 @@ function rel() {
                     while(a.tagName !== "LI") {
                         a = a.parentNode;
                     }
-                    a.remove();
+                    a.setAttribute("class", "del");
                 }
             }
         });
